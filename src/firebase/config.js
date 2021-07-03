250615f8f05f7fc588c21734d88f6659f185d82b
Brenda Wong <Brendot.wong@gmail.com>
2021-07-02 20:33:40 -0400

    initial commit

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,19 +1,23 @@
-import * as firebase from 'firebase';
-import '@firebase/auth';
-import '@firebase/firestore';
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAOWHBpPhKoNhcGFKHH_Q_0AtL2gV-imgQ',
-  authDomain: 'production-a9404.firebaseapp.com',
-  databaseURL: 'https://production-a9404.firebaseio.com',
-  projectId: 'production-a9404',
-  storageBucket: 'production-a9404.appspot.com',
-  messagingSenderId: '525472070731',
-  appId: '1:525472070731:web:ee873bd62c0deb7eba61ce',
+if (process.env.NODE_ENV !== "production") require("./secret_key");
+
+const FIRESTORE_API_KEY = process.env.FIRESTORE_API_KEY;
+
+export const firebaseConfig = {
+  apiKey: FIRESTORE_API_KEY,
+  authDomain: "pawsome-ce323.firebaseapp.com",
+  databaseURL: "https://pawsome-ce323.firebaseio.com",
+  projectId: "pawsome-ce323",
+  storageBucket: "pawsome-ce323.appspot.com",
+  messagingSenderId: "12345-insert-yourse",
+  appId: "1:277794075313:ios:5c7fdb570c124882e12890",
 };
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 export { firebase };
---
+++ src/firebase/secret_key.js
@@ -0,0 +1,7 @@
+process.env.FIRESTORE_API_KEY = "AIzaSyCdt5wiQDYGhiNPEC6TxwxX-EyGC4vLa70";
+process.env.FIRESTORE_AUTH_DOMAIN = "pawsome-ce323.firebaseapp.com";
+process.env.FIRESTORE_DATABASE_URL = "https://pawsome-ce323.firebaseio.com";
+process.env.FIRESTORE_PROJECT_ID = "pawsome-ce323";
+process.env.FIRESTORE_STORAGE_BUCKET = "pawsome-ce323.appspot.com";
+process.env.FIRESTORE_MESSAGING_SENDER_ID = "12345-insert-yourse";
+process.env.FIRESTORE_APP_ID = "1:277794075313:ios:5c7fdb570c124882e12890";
