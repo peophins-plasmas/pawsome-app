090d8127e95819f40878d2b6ef14c63e8bb50b1e
Rebekkah Jou <rebekkahniles@gmail.com>
2021-07-08 11:46:46 -0400

    fix: bugs fixed

--- src/Components/UploadImage.js
+++ src/Components/UploadImage.js
@@ -13,11 +13,9 @@ import { firebase } from "../firebase/config";
 
 if (process.env.NODE_ENV !== "production") require("../../secrets");
 
-<<<<<<< HEAD
 const checkForLibraryPermission = async () => {
-  const {
-    libraryPermission,
-  } = await ImagePicker.getMediaLibraryPermissionsAsync();
+  const { libraryPermission } =
+    await ImagePicker.getMediaLibraryPermissionsAsync();
   if (libraryPermission !== "granted") {
     alert("Please grant permission for this app to access your media library");
     await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -33,15 +31,6 @@ const checkForCameraPermission = async () => {
     await ImagePicker.requestCameraPermissionsAsync();
   } else {
     console.log("Camera permissions are granted");
-=======
-const checkForCameraRollPermission = async () => {
-  //need to pass users in as props to upload image component and check user permissions if false, have the alert render
-  const { status } = await ImagePicker.getMediaLibraryPermissionsAsync();
-  if (status !== "granted") {
-    alert("Please grant camera roll permissions inside your system's settings");
-  } else {
-    console.log("Media Permissions are granted");
->>>>>>> calevents
   }
 };
 
@@ -52,7 +41,6 @@ export default function UploadImage(props) {
   const [userImage, setUserImage] = useState("");
   const userImageRef = firebase.firestore().collection("users").doc(user.id);
 
-<<<<<<< HEAD
   //   useEffect(() => {
   //     checkForLibraryPermission();
   //     checkForCameraPermission();
@@ -60,13 +48,6 @@ export default function UploadImage(props) {
 
   const addImageFromLibrary = async () => {
     checkForLibraryPermission();
-=======
-  useEffect(() => {
-    checkForCameraRollPermission();
-  }, []);
-
-  const addImage = async () => {
->>>>>>> calevents
     let _image = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
@@ -93,10 +74,7 @@ export default function UploadImage(props) {
       method: "POST",
     }).then(async (r) => {
       let data = await r.json();
-<<<<<<< HEAD
       console.log("data in post response>>>>>", data);
-=======
->>>>>>> calevents
       setImage(data.url);
       return userImageRef
         .update({
@@ -111,9 +89,7 @@ export default function UploadImage(props) {
     });
   };
 
-<<<<<<< HEAD
   const captureImageFromCamera = async () => {
-    console.log("camera was clicked");
     checkForCameraPermission();
     let _image = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -164,33 +140,21 @@ export default function UploadImage(props) {
           style={imageUploaderStyles.uploadBtn}
         >
           <Text>{image ? "Take new photo from" : "Upload from"} Camera</Text>
-          <AntDesign name="camerao" size={20} color="black" />
+          <AntDesign name='camerao' size={20} color='black' />
         </TouchableOpacity>
       </View>
 
-=======
-  return (
-    <View style={imageUploaderStyles.container}>
->>>>>>> calevents
       {image && (
         <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
       )}
 
       <View style={imageUploaderStyles.uploadBtnContainer}>
         <TouchableOpacity
-<<<<<<< HEAD
           onPress={addImageFromLibrary}
           style={imageUploaderStyles.uploadBtn}
         >
           <Text>{image ? "Edit" : "Upload"} Image</Text>
-          <AntDesign name="clouduploado" size={20} color="black" />
-=======
-          onPress={addImage}
-          style={imageUploaderStyles.uploadBtn}
-        >
-          <Text>{image ? "Edit" : "Upload"} Image</Text>
-          <AntDesign name="camera" size={20} color="black" />
->>>>>>> calevents
+          <AntDesign name='clouduploado' size={20} color='black' />
         </TouchableOpacity>
       </View>
     </View>
@@ -222,7 +186,6 @@ const imageUploaderStyles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-<<<<<<< HEAD
   cameraBtnContainer: {
     opacity: 0.7,
     right: 0,
@@ -231,6 +194,4 @@ const imageUploaderStyles = StyleSheet.create({
     width: "100%",
     height: "25%",
   },
-=======
->>>>>>> calevents
 });
