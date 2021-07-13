781a69d87142225cbf8bc00f7a0bc023143e265f
Anna Vaigast <av1082@nyu.edu>
2021-07-13 11:22:33 -0400

    feat: fixed bug on adding pet

--- src/screens/UserScreen/petForm.js
+++ src/screens/UserScreen/petForm.js
@@ -5,6 +5,7 @@ import styles from './styles';
 import { Formik } from 'formik';
 import colors from '../combinedStyles'
 
+
 export default function PetForm(props) {
 
   const petsRef = firebase.firestore().collection("pets");
@@ -13,15 +14,18 @@ export default function PetForm(props) {
   return (
     <View style={styles.containerForm}>
       <Formik
-        initialValues={{ petName: '', species: '', weight: '', ownerId: userId }}
+        initialValues={{ petName: '', species: '', weight: '', ownerId: userId, image: 'https://res.cloudinary.com/dx5gk8aso/image/upload/v1625860768/1200px-Paw-print.svg_hmqdd7.png' }}
         onSubmit={(values, actions) => {
                   actions.resetForm();
                   petsRef.add({
                     ownerId: userId,
+                    image: values.image,
                     petName: values.petName,
                     species: values.species,
-                    weight: values.weight
+                    weight: values.weight,
+
         })
+        Alert.alert('Pet Submitted!')
       }}
       >
         {props => (
@@ -37,12 +41,10 @@ export default function PetForm(props) {
             />
             <TextInput
               style={styles.inputForm}
-              multiline
               placeholder='Dog? Cat? Bird?'
               onChangeText={props.handleChange('species')}
               value={props.values.species}
             />
-
             <TextInput
               style={styles.inputForm}
               placeholder='How much do I weigh?'
