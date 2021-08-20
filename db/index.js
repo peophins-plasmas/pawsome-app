468544866867017811e5f8e610d1590140b4c448
Brenda Wong <Brendot.wong@gmail.com>
2021-08-20 19:56:47 -0400

    entered field names for models

--- db/index.js
+++ db/index.js
@@ -7,6 +7,22 @@ const Task = require("./models/Task");
 const Medication = require("./models/Medication");
 //associations
 
+User.hasMany(Pet);
+Pet.belongsTo(User);
+
+User.hasOne(Vet);
+Vet.hasMany(User);
+Pet.hasOne(Vet);
+Vet.hasMany(Pet);
+
+User.hasMany(Task);
+Task.belongsTo(User);
+Pet.hasMany(Task);
+Task.belongsTo(Pet);
+
+Pet.hasMany(Medication);
+Medication.belongsTo(Pet);
+
 module.exports = {
   db,
   models: {
---
+++ db/models/User.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const User = db.define("User", {
+  username: {},
+  password: {},
+  firstName: {},
+  lastName: {},
+  email: {},
+  image: {},
+  address: {},
+  phone: {},
+  zip: {},
+  isOwner: {},
+  isPetSitter: {},
+});
